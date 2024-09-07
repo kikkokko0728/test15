fd0a9ab993beae634e3ebe64f25c5f88580d28a0
kikkokko0728 <kikkokko0728@gmail.com>
2024-09-07 22:26:08 +0900

    Add Unity WebGL build files

---
+++ Build/suitest.framework.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:35faff3c5be42b8bf2136a9b768546258b97955506201b313e120bc062e8b4ac
+size 380243
---
+++ Build/suitest.loader.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8197e307ff026d6f80f31938a283951c22da87fa787c34bef3965e09713679fb
+size 21837
